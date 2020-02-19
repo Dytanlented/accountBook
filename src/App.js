926591bf23e5b756ea6a3a6bcd2389dccb97b306
Dante <494941020@qq.com>
2020-02-19 10:09:22 -0800

    route and link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import "./bootstrap.css";
 import './App.css';
-
+import { BrowserRouter as Router , Route, Link} from "react-router-dom"
 
 import Home from "./containers/Home";
-
+import Create from "./containers/Create"
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/create">Create</Link>
+        <Link to="/edit/998">Edit</Link>
+      </ul>
+      <div className="App">
+        <Route path="/" exact component={Home} />
+        <Route path="/create" component={Create} />
+        <Route path="/edit/:id" component={Create} />
+      </div>
+    </Router>
   );
 }
 
